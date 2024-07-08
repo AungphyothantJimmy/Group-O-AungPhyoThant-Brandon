8c0a92ba8dc9400875bb0564816686edef6c06de
Jimmy <aungphyothant@uon.edu.au>
2024-07-08 16:22:58 +0800

    implementing CompanysController

---
+++ HeavenHome/Controllers/CompaniesController.cs
@@ -0,0 +1,24 @@
+﻿using HeavenHome.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace HeavenHome.Controllers
+{
+    public class CompaniesController : Controller
+    {
+        public readonly AppDbContext _context;
+
+        public CompaniesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var allcompanies = await _context.Companies.ToListAsync();
+            return View(allcompanies);
+        }
+    }
+}
