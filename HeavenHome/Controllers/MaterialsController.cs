3e6890c0b8814e9d081fd99463b5d845610c0680
Jimmy <aungphyothant@uon.edu.au>
2024-07-08 16:24:42 +0800

    Implementing MaterialsController

---
+++ HeavenHome/Controllers/MaterialsController.cs
@@ -0,0 +1,39 @@
+﻿using HeavenHome.Data;
+using HeavenHome.Data.Services;
+using Microsoft.AspNetCore.Mvc;
+using HeavenHome.Models;
+
+namespace HeavenHome.Controllers
+{
+    public class MaterialsController : Controller
+    {
+        private readonly IMaterialsService _service;
+
+        public MaterialsController(IMaterialsService service)
+        {
+            _service = service;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var data = await _service.GetAll();
+            return View(data); 
+        }
+
+        //Get: Materials/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("PictureURL,Name,Bio")]Material material)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(material);
+            }
+            _service.Add(material);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
