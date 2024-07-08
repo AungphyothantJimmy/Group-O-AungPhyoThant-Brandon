357b8e345ceef4e0d3442675fcffadd65224dd10
Jimmy <aungphyothant@uon.edu.au>
2024-07-08 16:25:30 +0800

    Implementing ProductsController

---
+++ HeavenHome/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using HeavenHome.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HeavenHome.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allProducts = await _context.Products.Include(n => n.Company).ToListAsync();
+            return View(allProducts);
+        }
+    }
+}
