d98e9457c9a0436e8ce227829e3f9e9d20f48e01
Jimmy <aungphyothant@uon.edu.au>
2024-07-11 17:28:31 +0800

    Updated MaterialsController

--- HeavenHome/Controllers/MaterialsController.cs
+++ HeavenHome/Controllers/MaterialsController.cs
@@ -2,6 +2,8 @@
 using HeavenHome.Data.Services;
 using Microsoft.AspNetCore.Mvc;
 using HeavenHome.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
 
 namespace HeavenHome.Controllers
 {
@@ -15,7 +17,7 @@ namespace HeavenHome.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var data = await _service.GetAll();
+            var data = await _service.GetAllAsync();
             return View(data); 
         }
 
@@ -26,13 +28,57 @@ namespace HeavenHome.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create([Bind("PictureURL,Name,Bio")]Material material)
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Create([Bind("Name,PictureURL,Bio")] Material material)
         {
-            if(!ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 return View(material);
             }
-            _service.Add(material);
+            await _service.AddAsync(material);
+            return RedirectToAction(nameof(Index));
+        }
+
+        //Get; Actors/Details/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var materialDetails = await _service.GetByIdAsync(id);
+
+            if (materialDetails == null) return View("NotFound");
+            return View(materialDetails);
+        }
+
+        //Get: Materials/Edit/1
+        public async Task<IActionResult> Edit(int id)
+        {
+            var materialDetails = await _service.GetByIdAsync(id);
+            if (materialDetails == null) return View("NotFound");
+            return View(materialDetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,PictureURL,Bio")] Material material)
+        {
+            await _service.UpdateAsync(id, material);
+            return RedirectToAction(nameof(Index));
+        }
+
+        //Get: Materials/Delete/1
+        public async Task<IActionResult> Delete(int id)
+        {
+            var materialDetails = await _service.GetByIdAsync(id);
+            if (materialDetails == null) return View("NotFound");
+            return View(materialDetails);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var materialDetails = await _service.GetByIdAsync(id);
+            if (materialDetails == null) return View("NotFound");
+
+            await _service.DeleteAsync(id);
             return RedirectToAction(nameof(Index));
         }
     }
