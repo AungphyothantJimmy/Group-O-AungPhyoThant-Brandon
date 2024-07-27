3457ae29ca3e68d232301e5a281390a79c347e38
Jimmy <138547038+AungphyothantJimmy@users.noreply.github.com>
2024-07-28 01:09:20 +0800

    Adding new services for companies and products

---
+++ HeavenHome/Data/Services/IProductsService.cs
@@ -0,0 +1,12 @@
+﻿using HeavenHome.Models;
+using HeavenHome.Data.Base;
+using HeavenHome.Models;
+using System.Linq.Expressions;
+
+namespace HeavenHome.Data.Services
+{
+    public interface IProductsService:IEntityBaseRepository<Product>
+    {
+        Task<Product> GetProductbyIdAsync(int id);
+    }
+}
---
+++ HeavenHome/Data/Services/ProductsService.cs
@@ -0,0 +1,26 @@
+﻿using HeavenHome.Data.Base;
+using HeavenHome.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace HeavenHome.Data.Services
+{
+    public class ProductsService:EntityBaseRepository<Product>, IProductsService
+    {
+        private readonly AppDbContext _context;
+        public ProductsService(AppDbContext context): base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> GetProductbyIdAsync(int id)
+        {
+            var productDetails = await _context.Products
+                .Include(c => c.Company)
+                .Include(am => am.Materials_Products).ThenInclude(a => a.Material)
+                .FirstOrDefaultAsync(n => n.Id == id);
+
+            return productDetails;
+        }
+    }
+}
